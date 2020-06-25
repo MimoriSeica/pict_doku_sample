1fa8f7ce403a9fa21d4596ba67e7f5cf0adeb3bc
MimoriSeica <zerons_0612@yahoo.co.jp>
2020-06-25 17:52:36 +0900

    数独の画面

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter
+import numpy as np
 from tkinter import colorchooser
 from PIL import Image, ImageDraw
 
@@ -7,6 +8,7 @@ class Application(tkinter.Frame):
         super().__init__(master)
         self.master = master
         self.master.title('tkinter canvas trial')
+        self.configure(background = '#DDEEEE')
         self.pencil_color = 'black'
         self.pack()
         self.create_widgets()
@@ -28,21 +30,26 @@ class Application(tkinter.Frame):
         self.save_button = tkinter.Button(self, text='save', command=self.save_canvas)
         self.save_button.grid(row=0, column=4)
 
-        self.test_canvas = tkinter.Canvas(self, bg='white', width=600, height=600)
-        self.test_canvas.grid(row=1, column=0, columnspan=4)
-        self.test_canvas.bind('<B1-Motion>', self.paint)
-        self.test_canvas.bind('<ButtonRelease-1>', self.reset)
+        self.paint_canvas = tkinter.Canvas(self, bg='white', width=600, height=600)
+        self.paint_canvas.grid(row=1, column=0, columnspan=4)
+        self.paint_canvas.bind('<B1-Motion>', self.paint)
+        self.paint_canvas.bind('<ButtonRelease-1>', self.reset)
+
+        self.sudoku_canvas = tkinter.Canvas(self, bg='white', width=600, height=600)
+        self.sudoku_canvas.grid(row=1, column=5, columnspan=4)
+
+        self.numbers = np.zeros((3, 3))
+        self.make_sudoku()
 
     def setup(self):
         self.old_x = None
         self.old_y = None
-        self.color = 'black'
         self.eraser_on = False
-        self.im = Image.new('RGB', (600, 600), 'white')
-        self.draw = ImageDraw.Draw(self.im)
+        self.clear_canvas()
 
     def change_color(self):
         self.pencil_color = colorchooser.askcolor()[1]
+        self.change_color_button.bg = self.pencil_color
 
     def change_radio(self):
         if self.vr.get() == 1:
@@ -51,7 +58,7 @@ class Application(tkinter.Frame):
             self.eraser_on = True
 
     def clear_canvas(self):
-        self.test_canvas.delete(tkinter.ALL)
+        self.paint_canvas.delete(tkinter.ALL)
         self.im = Image.new('RGB', (600, 600), 'white')
         self.draw = ImageDraw.Draw(self.im)
 
@@ -64,10 +71,25 @@ class Application(tkinter.Frame):
         else:
             paint_color = self.pencil_color
         if self.old_x and self.old_y:
-            self.test_canvas.create_line(self.old_x, self.old_y, event.x, event.y, width=5.0, fill=paint_color, capstyle=tkinter.ROUND, smooth=tkinter.TRUE, splinesteps=36)
+            self.paint_canvas.create_line(self.old_x, self.old_y, event.x, event.y, width=10.0, fill=paint_color, capstyle=tkinter.ROUND, smooth=tkinter.TRUE, splinesteps=36)
             self.draw.line((self.old_x, self.old_y, event.x, event.y), fill=paint_color, width=5)
         self.old_x = event.x
         self.old_y = event.y
+
+    def make_sudoku(self):
+        self.sudoku_canvas.delete(tkinter.ALL)
+        for x in range(30, 571, 60):
+            w = 5
+            if (x - 30) % 90 == 0:
+                w = 10.
+            self.sudoku_canvas.create_line(x, 30, x, 570, width=w, fill='black', capstyle=tkinter.ROUND, smooth=tkinter.TRUE, splinesteps=36)
+
+        for y in range(30, 571, 60):
+            w = 5
+            if (y - 30) % 90 == 0:
+                w = 10.
+            self.sudoku_canvas.create_line(30, y, 570, y, width=w, fill='black', capstyle=tkinter.ROUND, smooth=tkinter.TRUE, splinesteps=36)
+        self.sudoku_canvas.create_text(60, 60, text = '1', font = ('', 40))
 
     def reset(self, event):
         self.old_x, self.old_y = None, None
